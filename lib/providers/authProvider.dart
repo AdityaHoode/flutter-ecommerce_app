ca5cc46699c0b310cb65566cd48295546b375531
Aditya Hoode <adityahoode3@gmail.com>
2021-05-10 00:01:08 +0530

    #268

--- lib/providers/authProvider.dart
+++ lib/providers/authProvider.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/models/http_exception.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -11,17 +12,25 @@ class AuthProvider with ChangeNotifier {
   Future<void> signUp(String email, String password) async {
     final url = Uri.parse(
         "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${env['WEB_API_KEY']}");
-    final res = await http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        },
-      ),
-    );
-    print(json.decode(res.body));
+    try {
+      final res = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      print(json.decode(res.body));
+      final resData = json.decode(res.body);
+      if (resData['error'] != null) {
+        throw HttpException(resData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> login(String email, String password) async {
