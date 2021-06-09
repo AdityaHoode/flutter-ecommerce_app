669da3500355111c897e43309170794140a21509
Aditya Hoode <adityahoode3@gmail.com>
2021-06-09 16:48:01 +0530

    fix

--- lib/providers/authProvider.dart
+++ lib/providers/authProvider.dart
@@ -111,7 +111,7 @@ class AuthProvider with ChangeNotifier {
 
   Future<bool> tryAutoLogin() async {
     final prefs = await SharedPreferences.getInstance();
-    if (prefs.containsKey('userData')) {
+    if (!prefs.containsKey('userData')) {
       return false;
     }
     final extractedUserData =
